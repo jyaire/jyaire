61c958d87b946095f8ea825e3120c33dcbd1cb9f
Jean-Roch Masson <jyaire@gmail.com>
2020-09-26 16:54:46 +0200

    Add contact-line component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactLineComponent } from './contact-line/contact-line.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactLineComponent
   ],
   imports: [
     BrowserModule,
