5fb652f95f0cdc830243a5eb9e07444cde2e5e97
Jean-Roch Masson <jyaire@gmail.com>
2020-09-26 17:13:47 +0200

    Put first information in contact-line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactLineComponent } from './contact-line/contact-line.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactLineComponent
+    ContactLineComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
