45fe84ecc7b83bd79d2c975fea1723406bfb8975
Jean-Roch Masson <jyaire@gmail.com>
2020-10-21 21:39:24 +0200

    Add thunbails to portfolio page

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -20,29 +20,52 @@ export class PortfolioComponent implements OnInit {
     },
     {
       id: 2,
-      name: 'Ecole la Providence',
-      description: "Site de l'école maternelle et primaire la Providence, à Dunkerque",
-      link: 'http://jejoueenclasse.fr/classeroyale/',
+      name: 'BiBip Mobile !',
+      description: "Récupération de téléphones mobiles sur la région Hauts de France",
+      image: 'bipbip.png',
     },
     {
       id: 3,
+      name: 'Ecole la Providence',
+      description: "Site de l'école maternelle et primaire la Providence, à Dunkerque",
+      link: 'http://www.ecolelaprovidence.fr/',
+      image: 'providence.png',
+    },
+
+    {
+      id: 4,
       name: "Il faut que j'vous raconte",
       description: "Blog personnel",
       link: 'http://mapoupoule.free.fr',
+      image: 'mapoupoule.png',
     },
     {
-      id: 4,
+      id: 5,
+      name: "Elucubrations pédagogiques",
+      description: "Réflexions sur l'enseignement, l'éducation et la pédagogie",
+      link: 'http://jejoueenclasse.fr/elucubrations/',
+      image: 'elucubrations.png',
+    },
+    {
+      id: 6,
       name: "Les (bonnes) notes de JR",
       description: "Partage de ressources pour enseignants en Primaire",
       link: 'http://jejoueenclasse.fr/bonnesnotes/',
       image: 'bonnesnotes.png',
     },
     {
-      id: 4,
+      id: 7,
       name: "Rencontres chantantes",
       description: "Ressources pour chanter à l'école",
       link: 'http://doumdoumdoum.free.fr',
       image: 'doumdoumdoum.png',
+    },
+    {
+      id: 8,
+      name: "Les anciens de Beaucamps",
+      description: "Site des anciens élèves de l'institution Ste Marie de Beaucamps-Ligny (59)",
+      link: 'http://asmbeaucamps.free.fr/',
+      image: 'asmbeaucamps.png',
     },
   ]
 
